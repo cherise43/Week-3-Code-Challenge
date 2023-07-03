650ff1c274650e4bd24a9f0f2d76acac4706d3ec
Cherise <nashpaiessy@gmail.com>
2023-07-03 11:14:03 +0300

    completed assignment

---
+++ Readme.md
@@ -0,0 +1,39 @@
+# Title 
+Phase 1, week 3 Code Challenge
+
+# Description
+Flatdango Movie Theater 
+
+# Introduction 
+Flatdango is a movie theatre that allows people to purchase movie tickets.
+
+# Project SetUp 
+-Fork your repository
+-Clone this repository git@github.com:cherise43/Week-3-Code-Challenge.git on your terminal in ubuntu.
+-Open a visual studio code on your terminal by typing `code`.
+-Run this command to get the backend started:`json-server --watch db.json`
+-Test the server by visiting this route in the browser`http://localhost:3000/films`
+-Open a html file of the project and click on the live server to launch the program.
+- Click the button whereby the user can select the movie tickets including it's poster,title,runtime,showtime and available tickets when the page loads.
+
+# Requirements
+Ubuntu
+A Visual Studio Code
+Moderate Internet Connection
+
+# Author 
+Cathy Maseki
+
+# License
+MIT License
+Copyright (c) [2023] [Cathy Maseki]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,115 @@
+// Select the ul#films element
+constfilmList= document.querySelector('#films');
+// Fetch the movie data 
+fetch("http://localhost:3000/films")
+.then (function(response){
+    return response.json()
+})   
+.then(function(data){
+    // Loop through each movie in data and add it to the filmList
+    data.forEach(function(movie){
+        const listItem = document.createElement('li');
+        // Defining an object list item , assigning a document.create method to create a list when the data is looped from db.json
+        listItem.classList.add('film','item');
+        listItem.textContent= movie.title;
+        filmList.appendChild(listItem);
+    });
+    const films = document.querySelector('.body');
+    // Looping a new movie in the data and fetching a new card for it
+    data.forEach(function(movie){
+        const card = document.createElement('div');
+        card.classLIst.add('card');
+        card.innerHTML = `
+    < div class = "container">
+<img src="${movie.poster}" alt"product image"style = 'width:50% ;height:50%'>
+<div class="container">
+          <img src="${movie.poster}" alt="Product Image" style="width:50%; height:50%">
+          <h1><b>${movie.title}</b></h1>
+          <p>Runtime: ${movie.runtime}</p>
+          <p>Showtime: ${movie.showtime}</p>
+          <p>Available Tickets: <span class="available-tickets">${movie.capacity - movie.tickets_sold}</span></p>
+          <button class="buy-ticket">Buy Ticket</button>
+        </div>
+
+    </div>
+        `;
+        films.appendChild(card);
+    })
+})// Select the ul#films element
+const filmList = document.querySelector('#films');
+
+// Fetch the movie data
+fetch("http://localhost:3000/films")
+  .then(function (response) {
+    return response.json();
+  })
+.then(function (data) {
+    // Loop through each movie in the data and add it to the filmList
+    data.forEach(function (movie) {
+      const listItem = document.createElement('li');
+      listItem.classList.add('film', 'item');
+      listItem.textContent = movie.title;
+      filmList.appendChild(listItem);
+    });
+
+    // Select the .body element
+    const films = document.querySelector('.body');
+
+    // Loop through each movie in the data and create a new card for it
+    data.forEach(function (movie) {
+      const card = document.createElement('div');
+      card.classList.add('card');
+      card.innerHTML = `
+        <div class="container">
+          <img src="${movie.poster}" alt="Product Image" style="width:50%; height:50%">
+          <h1><b>${movie.title}</b></h1>
+          <p>Runtime: ${movie.runtime}</p>
+          <p>Showtime: ${movie.showtime}</p>
+          <p>Available Tickets: <span class="available-tickets">${movie.capacity - movie.tickets_sold}</span></p>
+          <button class="buy-ticket">Buy Ticket</button>
+        </div>
+      `;
+      films.appendChild(card);
+
+      // Add a click event listener to the "Buy Ticket" button
+      const buyButton = card.querySelector('.buy-ticket');
+      buyButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the page from reloading
+        // Check if there are any available tickets
+        const availableTickets = card.querySelector('.available-tickets');
+        const numAvailableTickets = parseInt(availableTickets.textContent);
+        if (numAvailableTickets > 0) {
+          // Update the number of available tickets and display it on the frontend
+          availableTickets.textContent = numAvailableTickets - 1;
+
+          // Update the movie data in the backend
+          const newTicketsSold = movie.tickets_sold + 1;
+          fetch('http://localhost:3000/films/${movie.id}', {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ tickets_sold: newTicketsSold })
+          })
+            .then(function(response) {
+              if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+            })
+            .catch(function(error) {
+              console.error('There was a problem updating the movie data:', error);
+            });
+        } else {
+          alert('Sorry, this showing is sold out!');
+        }
+      });
+    });
+  })("http://localhost:3000/films")
+  .catch(function(error) {
+    console.log('There was an error fetching the movie data:', error);
+  });
+
+ 
+    
+
+
